4d8f3554b5bdf2a3899025a37d0fab925468edbc
Vatsal Sharma <156673638+vatsalsharmaaa@users.noreply.github.com>
2025-12-29 22:34:56 +0530

    Time: 11 ms (23.88%) | Memory: 20.1 MB (36.20%) - LeetSync

---
+++ 207-course-schedule/course-schedule.cpp
@@ -0,0 +1,47 @@
+class Solution {
+public:
+
+    bool toposort(vector<vector<int>>&adj,int n,vector<int>& indegree)
+    {   int count =0;
+        queue<int> q;
+        for(int i=0;i<n;i++){
+            if(indegree[i]==0){
+                count++;
+            q.push(i);
+            }
+        }
+        while(!q.empty()){
+            int temp= q.front();
+            q.pop();
+
+            for(auto v: adj[temp]){
+                indegree[v]--;
+                if(indegree[v]==0){
+                    count++;
+                    q.push(v);
+                }
+            }
+
+        }
+        if(count==n) return true;
+
+        return false;
+
+    }
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        vector<vector<int>>adj(numCourses);
+        vector<int> indegree(numCourses,0);//kahn algo
+
+        for(auto x: prerequisites){
+            int a=x[0];
+            int b=x[1];
+            adj[b].push_back(a);
+            //0---->1
+            indegree[a]++;
+        }
+
+        //if cycle is present toh possible hi nahi hai
+
+        return toposort(adj,numCourses,indegree);
+    }
+};
