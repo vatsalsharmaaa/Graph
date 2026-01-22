a3af0d36083ac54db5058b02be302668742f6919
Vatsal Sharma <156673638+vatsalsharmaaa@users.noreply.github.com>
2026-01-22 10:11:33 +0530

    Time: 0 ms (100.00%) | Memory: 9.6 MB (16.80%) - LeetSync

--- 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.cpp
+++ 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.cpp
@@ -1,29 +1,119 @@
 class Solution {
-public:
-    vector<string> result;
-    vector<string> mp = {"", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
 
-    void solve(string &digits, int idx, string &curr) {
-        if (idx == digits.length()) {
+    vector<string> result;
+    vector<string > mp ={ "","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
+public:
+
+    void solve(string &digits,int idx,string &curr){
+
+        if(idx==digits.length()){
             result.push_back(curr);
-            return;
+            return ;
+
         }
 
-        int num = digits[idx] - '0';
-        string alpha = mp[num];
+        int num= digits[idx]-'0';
+        string alpha= mp[num];
 
-        for (char c : alpha) {
+        for(char c: alpha){
+
             curr.push_back(c);
-            solve(digits, idx + 1, curr);
+            solve(digits,idx+1,curr);
             curr.pop_back();
         }
     }
-
     vector<string> letterCombinations(string digits) {
-        if (digits.empty()) return {};
 
-        string curr = "";
-        solve(digits, 0, curr);
+        string curr="";
+
+        solve(digits,0,curr);
         return result;
+        
     }
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// class Solution {
+// public:
+//     vector<string> result;
+//     vector<string> mp = {"", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+
+//     void solve(string &digits, int idx, string &curr) {
+//         if (idx == digits.length()) {
+//             result.push_back(curr);
+//             return;
+//         }
+//         int num = digits[idx] - '0';
+//         string alpha = mp[num];
+
+//         for (char c : alpha) {
+//             curr.push_back(c);
+//             solve(digits, idx + 1, curr);
+//             curr.pop_back();
+//         }
+//     }
+//     vector<string> letterCombinations(string digits) {
+//         if (digits.empty()) return {};
+//         string curr = "";
+//         solve(digits, 0, curr);
+//         return result;
+//     }
+// };
