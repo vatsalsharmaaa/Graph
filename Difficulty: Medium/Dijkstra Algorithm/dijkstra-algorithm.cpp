f3894dc412657502cd61b4ae436ffad950eac028
Vatsal Sharma <156673638+vatsalsharmaaa@users.noreply.github.com>
2026-06-08 23:57:05 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Dijkstra Algorithm/dijkstra-algorithm.cpp
+++ Difficulty: Medium/Dijkstra Algorithm/dijkstra-algorithm.cpp
@@ -1,37 +1,33 @@
-typedef pair<int,int> p;
-
 class Solution {
   public:
-    vector<int> dijkstra(int v, vector<vector<int>> &edges, int src) {
-        
-        vector<vector<p>> adj(v);
-        for (auto &e : edges) {
-            adj[e[0]].push_back({e[1], e[2]});
-            adj[e[1]].push_back({e[0], e[2]});
+    
+    typedef pair<int ,int>p;
+    vector<int> dijkstra(int V, vector<vector<int>> &edges, int src) {
+        vector<vector<p>>adj(V);
+        for(auto &e: edges){
+            adj[e[0]].push_back({e[1],e[2]});
+            adj[e[1]].push_back({e[0],e[2]});
         }
+        vector<int>result(V,INT_MAX);
+        priority_queue<p,vector<p>,greater<p>>pq;
+        result[src]=0;
+        pq.push({0,src});
         
-        vector<int> result(v, INT_MAX);
-        priority_queue<p, vector<p>, greater<p>> pq;
-        
-        result[src] = 0;
-        pq.push({0, src});
-        
-        while (!pq.empty()) {
-            int wt = pq.top().first;
-            int node = pq.top().second;
+        while(!pq.empty()){
+            int wt=pq.top().first;
+            int node=pq.top().second;
             pq.pop();
-            
-            for (auto &x : adj[node]) {
-                int nw_node = x.first;
-                int nw = x.second;
+            for(auto &x:adj[node]){
+                int n_node=x.first;
+                int nw=x.second;
                 
-                if (wt + nw < result[nw_node]) {
-                    result[nw_node] = wt + nw;
-                    pq.push({wt + nw, nw_node}); 
+                if(wt+nw<result[n_node]){
+                    result[n_node]=wt+nw;
+                    pq.push({wt+nw,n_node});
                 }
             }
         }
         return result;
+        
     }
 };
-
