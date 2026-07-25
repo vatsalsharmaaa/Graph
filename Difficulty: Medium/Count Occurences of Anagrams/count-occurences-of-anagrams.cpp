4752bc9737c300e286729e7242f62449a3138388
Vatsal Sharma <156673638+vatsalsharmaaa@users.noreply.github.com>
2026-07-25 22:37:01 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Count Occurences of Anagrams/count-occurences-of-anagrams.cpp
+++ Difficulty: Medium/Count Occurences of Anagrams/count-occurences-of-anagrams.cpp
@@ -1,44 +1,48 @@
 class Solution {
-  public:
-  
-  bool allzero(vector<int>&freq){
-      
-      for(auto i: freq){
-          if(i!=0) return false;
-      }
-      return true;
-  }
-    int search(string &pat, string &txt) {
-        // code here
-        int n=txt.length();
-        
-        
-        vector<int>freq(26,0);
-        
-        for(int i=0;i<pat.length();i++){
-            int ch=pat[i];
-            
-            freq[ch-'a']++;
-            
-        }
-       
-        int i=0;
-        int j=0;
-        int k=pat.size();
-        int count=0;
-        
-        while(j<n){
-            
-            freq [ txt[j] - 'a' ]--;
-            if(j-i+1==k){ 
-                if(allzero(freq)) count++;
-                
-                freq[txt[i]-'a']++;
-                i++;
+public:
+
+    bool allzero(vector<int>& arr) {
+        for (int i = 0; i < arr.size(); i++) {
+            if (arr[i] != 0) {
+                return false;
             }
-            j++;
-            
         }
-        return count;
+        return true;
+    }
+
+    int search(string &pat, string &txt) {
+        int n = pat.size();
+
+        vector<int> arr(26, 0);
+
+        int i = 0;
+        int j = 0;
+        int result = 0;
+
+        for (int k = 0; k < n; k++) {
+            arr[pat[k] - 'a']++;
+        }
+
+        while (j < txt.size()) {
+
+            arr[txt[j] - 'a']--;
+
+            if (j - i + 1 < n) {
+                j++;
+            }
+
+            else if (j - i + 1 == n) {
+
+                if (allzero(arr)) {
+                    result++;
+                }
+
+                arr[txt[i] - 'a']++;
+                i++;
+                j++;
+            }
+        }
+
+        return result;
     }
 };
