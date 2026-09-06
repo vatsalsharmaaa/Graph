3419d61182ce567438221dbbcf474790ef86be1f
Vatsal Sharma <156673638+vatsalsharmaaa@users.noreply.github.com>
2026-09-06 12:53:39 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Dijkstra Algorithm/dijkstra-algorithm.cpp
+++ Difficulty: Medium/Dijkstra Algorithm/dijkstra-algorithm.cpp
@@ -1,33 +1,53 @@
 class Solution {
   public:
-    
-    typedef pair<int ,int>p;
-    vector<int> dijkstra(int V, vector<vector<int>> &edges, int src) {
-        vector<vector<p>>adj(V);
-        for(auto &e: edges){
-            adj[e[0]].push_back({e[1],e[2]});
-            adj[e[1]].push_back({e[0],e[2]});
-        }
-        vector<int>result(V,INT_MAX);
-        priority_queue<p,vector<p>,greater<p>>pq;
-        result[src]=0;
-        pq.push({0,src});
-        
-        while(!pq.empty()){
-            int wt=pq.top().first;
-            int node=pq.top().second;
+    typedef pair<int,int> p;
+
+    void solve(int V,
+               vector<vector<pair<int,int>>> &adj,
+               vector<int> &res,
+               int src) {
+
+        priority_queue<p, vector<p>, greater<p>> pq;
+
+        pq.push({0, src});
+        res[src] = 0;
+
+        while(!pq.empty()) {
+
+            int weigh = pq.top().first;
+            int node = pq.top().second;
             pq.pop();
-            for(auto &x:adj[node]){
-                int n_node=x.first;
-                int nw=x.second;
-                
-                if(wt+nw<result[n_node]){
-                    result[n_node]=wt+nw;
-                    pq.push({wt+nw,n_node});
+
+            for(auto vec : adj[node]) {
+
+                int nnode = vec.first;
+                int nd = vec.second;
+
+                if(weigh + nd < res[nnode]) {
+                    res[nnode] = weigh + nd;
+                    pq.push({weigh + nd, nnode});
                 }
             }
         }
-        return result;
-        
+    }
+
+    vector<int> dijkstra(int V, vector<vector<int>> &edges, int src) {
+
+        vector<vector<pair<int,int>>> adj(V);
+
+        for(auto x : edges) {
+            int u = x[0];
+            int v = x[1];
+            int w = x[2];
+
+            adj[u].push_back({v, w});
+            adj[v].push_back({u, w});
+        }
+
+        vector<int> res(V, INT_MAX);
+
+        solve(V, adj, res, src);
+
+        return res;
     }
 };
